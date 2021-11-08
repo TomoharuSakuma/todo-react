a89b4f577ec7bccd4aae5fb28a01a17b69863696
kunakori <89243279+TomoharuSakuma@users.noreply.github.com>
2021-11-08 17:53:06 +0900

    props と state まで

---
+++ src/components/ColorfulMessage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ColorfulMessage = (props) => {
+  const { color, children } = props;
+  const contentStyle = { color: color, fontSize: "18px" };
+
+  // タグの中身を参照するには props.children
+  return <p style={contentStyle}>{children}</p>;
+};
+
+export default ColorfulMessage;
